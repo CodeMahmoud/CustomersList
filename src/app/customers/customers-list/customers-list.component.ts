70ead28ad705ce4eccf67f28766ab98b2aed8b81
CodeMahmoud <mahmoudcode913@gmail.com>
2023-06-05 13:56:44 -0500

    Updated code to work with services

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { ICustomer } from 'src/app/shared/interfaces';
 
@@ -7,13 +7,48 @@ import { ICustomer } from 'src/app/shared/interfaces';
   templateUrl: './customers-list.component.html'
 })
 export class CustomersListComponent implements OnInit {
-  filteredCustomer: ICustomer[] = [];
+  private _customers: ICustomer[] = [];
+
+  @Input() get customers(): ICustomer[] {
+    return this._customers;
+  }
+
+  set customers(value: ICustomer[]) {
+    if (value) {
+      this.filteredCustomers = this._customers = value;
+      this.calculateOrders();
+    }
+  }
+  filteredCustomers: any[] = [];
   customersOrderTotal!: number;
-  currenyCode: string = 'USD';
+  currencyCode: string = 'USD';
 
   constructor() {}
 
   ngOnInit(){
+
+  }
+
+  calculateOrders() {
+    this.customersOrderTotal = 0;
+    this.filteredCustomers.forEach((customer: ICustomer) => {
+      this.customersOrderTotal += customer.orderTotal;
+    });
+  }
+
+  filter(data: string) {
+    if (data) {
+      this.filteredCustomers = this.customers.filter((customer: ICustomer) => {
+        return customer.name.toLocaleLowerCase().indexOf(data.toLowerCase()) > -1 ||
+               customer.city.toLocaleLowerCase().indexOf(data.toLowerCase()) > -1 ||
+               customer.orderTotal.toString().indexOf(data) > -1;
+      });
+    } else {
+      this.filteredCustomers = this.customers;
+    }
+    this.calculateOrders();
+  }
+  sort(prop: string) {
 
   }
 }
--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -4,8 +4,7 @@ import { ICustomer } from '../shared/interfaces';
 
 @Component({
   selector: 'app-customers',
-  templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.css']
+  templateUrl: './customers.component.html'
 })
 export class CustomersComponent implements OnInit {
   title!: string;
@@ -15,7 +14,7 @@ export class CustomersComponent implements OnInit {
 
   ngOnInit(){
     this.title = 'Customers';
-      this.people = [
+    this.people = [
             { id: 1, name: 'john Doe', city: 'Phoenix', orderTotal: 9.99, customerSince: new Date(2014, 7, 10) },
             { id: 2, name: 'Jane Doe', city: 'Chandler', orderTotal: 19.99, customerSince: new Date(2017, 2, 22)},
             { id: 3, name: 'Michelle Thomas', city: 'Seattle', orderTotal: 99.99, customerSince: new Date(2002, 10, 31)},
